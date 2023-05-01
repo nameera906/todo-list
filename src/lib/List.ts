f53cda923429dc503970e2d618f4b09f8602a13a
yashweblife <yashweblife20@gmail.com>
2023-04-30 21:14:39 -0500

    add todo comments

--- src/lib/List.ts
+++ src/lib/List.ts
@@ -1,5 +1,9 @@
 import { Item } from "./Item";
-
+/**
+ * TODO: Add a comment to explain what the list class is
+ * TODO: Add a function to remove an item
+ * TODO: Add a button store the items data into localStorage
+ */
 export class List {
     public items: Item[] = [];
     public dom: HTMLElement = document.createElement('div');
