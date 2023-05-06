50da5824c053c1d67f7fc8bdf350ecf80cf17148
nameera906 <82439659+nameera906@users.noreply.github.com>
2023-05-06 20:01:54 +0530

    display message

--- src/lib/Item.ts
+++ src/lib/Item.ts
@@ -2,6 +2,7 @@ export class Item {
     public title: string = '';
     public completed: boolean = false;
     public dom: HTMLElement = document.createElement('div')
+    private messageDom: HTMLElement = document.createElement('p');
 
     constructor (parent: HTMLElement, title: string) {
         parent.append(this.dom)
@@ -18,17 +19,21 @@ export class Item {
         completeButton.addEventListener('click', () => {
           this.completed = true
           this.dom.classList.add('completed')
+          this.messageDom.innerHTML = 'Task has been completed.';
+          this.messageDom.style.display = 'block';
         })
         let incompleteButton = document.createElement('button')
         incompleteButton.innerHTML = 'not complete'
         incompleteButton.addEventListener('click', () => {
           this.completed = false
           this.dom.classList.remove('completed')
+          this.messageDom.innerHTML = 'Task has not been completed';
+          this.messageDom.style.display = 'block';
         })
         let t2 = document.createElement('h1')
         t2.innerHTML = title
     
-        this.dom.append(completeButton, incompleteButton, t2)
+        this.dom.append(completeButton, incompleteButton, t2, this.messageDom)
       }
     
 
