48a97e098a136348e0fd8df24cba8b5916fbf41c
nameera906 <82439659+nameera906@users.noreply.github.com>
2023-05-06 19:57:58 +0530

    add button

--- src/lib/Item.ts
+++ src/lib/Item.ts
@@ -10,14 +10,37 @@ export class Item {
     }
 
 
-    public setUpDom (title: string) {
+    public setUpDom(title: string) {
         let br = document.createElement('br')
-        br.innerHTML= ' '
-        let button = document.createElement('button')
-        button.innerHTML = 'complete';
+        br.innerHTML = ' '
+        let completeButton = document.createElement('button')
+        completeButton.innerHTML = 'complete'
+        completeButton.addEventListener('click', () => {
+          this.completed = true
+          this.dom.classList.add('completed')
+        })
+        let incompleteButton = document.createElement('button')
+        incompleteButton.innerHTML = 'not complete'
+        incompleteButton.addEventListener('click', () => {
+          this.completed = false
+          this.dom.classList.remove('completed')
+        })
         let t2 = document.createElement('h1')
-        t2.innerHTML = title 
+        t2.innerHTML = title
+    
+        this.dom.append(completeButton, incompleteButton, t2)
+      }
+    
 
-        this.dom.append(button, title)
-    }
+
+    // public setUpDom (title: string) {
+    //     let br = document.createElement('br')
+    //     br.innerHTML= ' '
+    //     let button = document.createElement('button')
+    //     button.innerHTML = 'complete';       
+    //     let t2 = document.createElement('h1')
+    //     t2.innerHTML = title 
+
+    //     this.dom.append(button, title)
+    // }
 }
